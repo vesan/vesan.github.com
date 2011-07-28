3d820adf936b8c692f6f89a438336c91d95ded69
Vesa Vänskä <vesan2000@gmail.com>
2011-07-28 17:22:46 +0300

    Adds jquery-inline-footnotes plugin

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,70 +1,98 @@
-// this script requires jQuery
-$(document).ready(function() {
-    Footnotes.setup();
-});
-
-var Footnotes = {
-  footnotetimeout: false,
-  setup: function() {
-    var footnotelinks = $("a[rel='footnote']");
-
-    footnotelinks.unbind('mouseover', Footnotes.footnoteover);
-    footnotelinks.unbind('mouseout', Footnotes.footnoteoout);
-
-    footnotelinks.bind('mouseover', Footnotes.footnoteover);
-    footnotelinks.bind('mouseout', Footnotes.footnoteoout);
-  },
-  footnoteover: function() {
-    clearTimeout(Footnotes.footnotetimeout);
-    $('#footnotediv').stop();
-    $('#footnotediv').remove();
-
-    var id = $(this).attr('href').substr(1);
-    var position = $(this).offset();
-
-    var div = $(document.createElement('div'));
-    div.attr('id', 'footnotediv');
-    div.bind('mouseover', Footnotes.divover);
-    div.bind('mouseout', Footnotes.footnoteoout);
-
-    var el = document.getElementById(id);
-    div.html($(el).html());
-
-    div.css({
-      position: 'absolute',
-      width: '400px',
-      opacity: 0.9
-    });
-    $(document.body).append(div);
-
-    var left = position.left;
-    if (left + 420 > $(window).width() + $(window).scrollLeft())
-    left = $(window).width() - 420 + $(window).scrollLeft();
-    var top = position.top + 20;
-    if (top + div.height() > $(window).height() + $(window).scrollTop())
-    top = position.top - div.height() - 15;
-    div.css({
-      left: left,
-      top: top
-    });
-  },
-  footnoteoout: function() {
-    Footnotes.footnotetimeout = setTimeout(function() {
-        $('#footnotediv').animate({
-            opacity: 0
-        },
-        600,
-        function() {
-            $('#footnotediv').remove();
+(function() {
+  /*
+  jQuery Inline Footnotes v1.0
+  Released under the MIT License.
+  */  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  (function($) {
+    $.inlineFootnote = function(el, options) {
+      this.el = $(el);
+      this.el.data("inlineFootnote", this);
+      this.initialize = function() {
+        this.options = $.extend({}, $.inlineFootnote.defaultOptions, options);
+        this.footnoteId = this.el.attr("href").match(/#(.*)/)[1];
+        if (this.footnoteId) {
+          this.el.mouseenter(this.openModal);
+          return $("body").mousemove(this.closeModal);
+        }
+      };
+      this.openModal = __bind(function(event) {
+        var footnoteContent, linkOffset;
+        if (!this.modal) {
+          footnoteContent = $("[id='" + this.footnoteId + "']").children().not(this.options.hideFromContent);
+          linkOffset = this.el.offset();
+          this.modal = $("<div />", {
+            id: this.options.modalId,
+            html: footnoteContent.clone(),
+            css: {
+              position: "absolute",
+              top: linkOffset.top,
+              left: linkOffset.left + this.el.outerWidth()
+            }
+          }).appendTo("body");
+          return this.positionModal();
+        }
+      }, this);
+      this.closeModal = __bind(function(event) {
+        if (this.modal) {
+          if (this.isHoveringFootnote(event)) {
+            clearTimeout(this.closeTimeout);
+            return this.closeTimeout = null;
+          } else {
+            if (!this.closeTimeout) {
+              return this.closeTimeout = setTimeout((__bind(function() {
+                this.modal.remove();
+                return this.modal = null;
+              }, this)), this.options.hideDelay);
+            }
+          }
+        }
+      }, this);
+      this.isHoveringFootnote = function(event) {
+        return this.modal.is(event.target) || $(event.target).closest(this.modal).length > 0 || event.target === this.el[0];
+      };
+      this.positionModal = function() {
+        var boxLeft, boxWidth, linkLeftOffset, modalHorizontalPadding, windowWidth;
+        modalHorizontalPadding = parseInt(this.modal.css("padding-left")) + parseInt(this.modal.css("padding-right"));
+        linkLeftOffset = this.el.offset().left;
+        windowWidth = $(window).width();
+        if ((windowWidth / 2) > linkLeftOffset) {
+          boxLeft = linkLeftOffset + 20;
+          boxWidth = windowWidth - boxLeft - modalHorizontalPadding - this.options.boxMargin * 2;
+          if (boxWidth > this.options.maximumBoxWidth) {
+            boxWidth = this.options.maximumBoxWidth;
+          }
+        } else {
+          boxWidth = linkLeftOffset - modalHorizontalPadding - this.options.boxMargin * 2;
+          if (boxWidth > this.options.maximumBoxWidth) {
+            boxWidth = this.options.maximumBoxWidth;
+          }
+          boxLeft = linkLeftOffset - boxWidth - this.options.boxMargin * 2;
+        }
+        return this.modal.css({
+          width: boxWidth,
+          left: boxLeft
         });
-    },
-    100);
-  },
-  divover: function() {
-    clearTimeout(Footnotes.footnotetimeout);
-    $('#footnotediv').stop();
-    $('#footnotediv').css({
-        opacity: 0.9
+      };
+      return this.initialize();
+    };
+    $.inlineFootnote.defaultOptions = {
+      boxMargin: 20,
+      hideDelay: 200,
+      hideFromContent: "[rev=footnote]",
+      maximumBoxWidth: 500,
+      modalId: "footnote_box"
+    };
+    return $.fn.inlineFootnote = function(options) {
+      return this.each(function() {
+        return new $.inlineFootnote(this, options);
+      });
+    };
+  })(jQuery);
+
+  $(function() {
+    $("[rel=footnote]").inlineFootnote({
+      maximumBoxWidth: 400
     });
-  }
-};
+  });
+}).call(this);
+
